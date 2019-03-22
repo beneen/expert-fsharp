4cbfb2e0c88858422db6887f95682378b79b4572
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-22 22:42:06 +0000

    record types

---
+++ Types/PatternMatchingRecords.fs
@@ -0,0 +1,32 @@
+﻿module PatternMatchingRecords
+
+// type representing a couple
+
+type Couple = { him : string ; her : string }
+
+// list of couples
+
+let couples = 
+    [ { him = "brad"; her = "angelina"};
+      { him = "becks" ; her = "posh" };
+      { him = "chris" ; her = "gwyneth"};
+      { him = "michael" ; her = "catherine" } ]
+
+// function to find david from a list of couples
+
+let rec findDavid l = 
+    match l with
+    | { him = x ; her = "posh"} :: tail -> x
+    | _ :: tail -> findDavid tail
+    | [] -> failwith "couldn't find david"
+
+let rec findPartner soughtHer l = 
+    match l with
+    | { him = x; her = her } :: tail when her = soughtHer -> x
+    | _ :: tail -> findPartner soughtHer tail
+    | [] -> failwith "couldnt find him"
+
+// print
+
+printfn "%A" (findDavid couples)
+printfn "%A" (findPartner "posh" couples)
--- Types/RecordTypesExample.fs
+++ Types/RecordTypesExample.fs
@@ -13,14 +13,20 @@ let rainbow = { boss = "Jeffrey"; lackeys = ["Zippy"; "George"; "Bungle"] }
 type Organisation2 = { cheif : string; underlings : string list}
 type Organisation3 = { cheif: string; indians: string list}
 
-// create instance of organisation2
+// create instance of organisation2 with type hint
 let (thePlayers: Organisation2) = 
     { cheif = "Peter Quince";
         underlings = ["Francis Flute"; "Robin Starveling"; "Tom Snout"; "Snug"; "Nick Bottom"]}
 
-// create an instance of Organisation3
-
+// create an instance of Organisation3 with type hint
 let (wayneManor: Organisation3) = 
     { cheif = "Batman";
         indians = ["Robin"; "Alfred"]}
+
+// create a modified instance of this type - update using with
+let wayneManor' = {wayneManor with indians = ["Alfred"]}
+
+printfn "wayneManor = %A" wayneManor
+printfn "wayneManor' = %A" wayneManor'
+
 
