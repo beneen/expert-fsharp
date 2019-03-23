cba5ba92a670a3719f3ad17c030ed4380030313a
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-23 21:17:08 +0000

    exception handling - exceptions are expensive to thow in f#, performance costs may not be worth using them for normal control flow

---
+++ ExceptionsAndExceptionHandling/RaisingExceptionsFinally.fs
@@ -0,0 +1,20 @@
+﻿module RaisingExceptionsFinally
+
+// finally cannot be used with with in f#
+// finally expression is excecuted whether or not an exception is thrown
+
+// function to write to a file
+
+let writeToFile() = 
+    // open a file
+    let file = System.IO.File.CreateText("test.txt")
+    try
+        // write to it
+        file.WriteLine("Hello F# users")
+    finally
+        // close the file, this will happen even if an exception occurs writing to the file
+        file.Dispose()
+// call the function
+
+writeToFile()
+
