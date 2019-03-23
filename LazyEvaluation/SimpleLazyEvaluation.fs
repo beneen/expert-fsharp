d31c3977c60a368c7ff2f5def5d26419da8aa756
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-23 22:06:38 +0000

    lazy evaluation

---
+++ LazyEvaluation/SimpleLazyEvaluation.fs
@@ -0,0 +1,21 @@
+﻿module SimpleLazyEvaluation
+
+let lazyValue = lazy (2 + 2)
+let actualValue = Lazy.force lazyValue
+
+printfn "%i" actualValue
+
+let lazySideEffect = 
+    lazy
+        (let temp = 2 + 2
+         printfn "%i" temp
+         temp)
+
+printfn "Force the value the first time: "
+let actualValue1 = Lazy.force lazySideEffect
+printfn "Force Value second time: "
+let actualValue 2 = Lazy.force lazySideEffect
+
+
+
+
