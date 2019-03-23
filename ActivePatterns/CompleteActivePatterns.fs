fb38a7d20e8020b2a080c6bfc2ad1e1bd98df5b8
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-23 17:17:53 +0000

    testing out partial active patterns and complete active patterns

--- ActivePatterns/CompleteActivePatterns.fs
+++ ActivePatterns/CompleteActivePatterns.fs
@@ -1,2 +1,46 @@
 ﻿module CompleteActivePatterns
+open System
 
+// similar syntax to defining a function.
+// the point of this is to execute a function to see if a match has occured or not.
+// design goal is to be able to permit better reuse of pattern matching logic.
+// all active patterns take an input and then perform some computation with that input
+// to determine whether a match has occured. there are two sorts:
+// Complete active patterns allow you to rbeak the match down into a finite number of cases
+// Partial active patterns can euther match or fail
+
+
+// syntax similar to defining a function but the identifier that represents an active pattern is surrounded by banana brackets
+// the names of the different cases of active pattern go between the banan brackets
+// the body of the active pattern is just an F# function that returns each case of the active pattern in the given banana brackets
+// each case may also return additional data, such as union types
+
+// the following parses string input data
+
+let (|Bool|Int|Float|String|) input = 
+    // attempt to parse a bool
+    let success, res = Boolean.TryParse input
+    if success then Bool(res)
+    else
+        // attempt to parse an int
+        let success, res = Int32.TryParse input
+        if success then Int(res)
+        else
+            // attempt to parse a float (double)
+            let success, res = Double.TryParse input 
+            if success then Float(res)
+            else String(input)
+
+// function to print the results by pattern matching over the active pattern
+
+let printInputWithType input = 
+    match input with
+    | Bool b -> printfn "Boolean: %b" b
+    | Int i -> printfn "Integer: %i" i
+    | Float f -> printfn " Floating point: %f" f
+    | String s -> printfn "String: %s" s
+
+// print the results
+printInputWithType "true"
+printInputWithType "12"
+printInputWithType "-12.1"
---
+++ ActivePatterns/PartialActivePattern.fs
@@ -0,0 +1,42 @@
+﻿module PartialActivePattern
+open System.Text.RegularExpressions
+open System
+
+// use similar syntax to a complete active pattern 
+// a partial active pattern has only one case name which is placed between banana brackets
+// a partial active pattern must be followed by a vertical bar and an underscore to show it is partial (rather than a complete active pattern with one case)
+
+// partial patterns are designed to wither match or fail to match, it is for this reason that they are option type
+// the option type is a simple union type already built into f#, it has wto cases: Some and None and it has the following definition
+
+//type option <'a> = 
+//    |Some of 'a // A partial active pattern will return Some along with any data to be returned
+//    | None // or None, which represents a failure
+
+
+// reimplementing problem from CompleteActivePattern.fs using partial active patterns
+
+// the definition of the active pattern
+let (|Regex|_|) regexPattern input = 
+    // create and attempt to match a regular expression
+    let regex = new Regex(regexPattern)
+    let regexMatch = regex.Match(input)
+    // return either Some or None
+    if regexMatch.Success then
+        Some regexMatch.Value
+    else
+        None
+
+// function to print the results by pattern matching over different instances of the active pattern
+
+let printInputWithType input = 
+    match input with 
+    | Regex "$true|false^" s -> printfn "Boolean: %s" s
+    | Regex @"$-?\d+^" s -> printfn "Integer: %s" s
+    | Regex "$-?\d+\.\d*^" s -> printfn "Floating point: %s" s
+    | _ -> printfn "String: %s" input
+
+// print
+printInputWithType "true"
+printInputWithType "12"
+printInputWithType "-12.1"
