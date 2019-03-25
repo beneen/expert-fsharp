6bcce7ab9af9feb3c3d44f120d18a3edc9210590
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-25 16:38:52 +0000

    More lazy evaluation examples

---
+++ LazyEvaluation/LazyFibonacci.fs
@@ -0,0 +1,15 @@
+﻿module LazyFibonacci
+
+// create an infinite list of fibonacci numbers
+
+let fibs = 
+    Seq.unfold  
+        (fun (n0, n1) ->
+            Some(n0, (n1, n0 + n1)))
+        (1I, 1I)
+// take the first 20 items from the list
+
+let first20 = Seq.take 20 fibs
+
+// print the finite list
+first20 |> Seq.iter (printf "%A ")
---
+++ LazyEvaluation/LazyListDefinition.fs
@@ -0,0 +1,19 @@
+﻿module LazyListDefinition
+
+
+// the lazy lsit definition
+
+let lazyList = 
+    Seq.unfold
+        (fun x ->
+            if x < 13 then
+                // if smaller than the limit return
+                // the current and next value
+                Some(x, x + 1 )
+            else
+                // if greater than the limit
+                // terminate the sequence
+                None)
+        10
+// print
+printfn "%A" lazyList
