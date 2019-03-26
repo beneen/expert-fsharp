01b5fed768066af07bbaff5841351a502f3f48dc
Beneen Tahir <beneen.tahir@gmail.com>
2019-03-26 22:31:05 +0000

    using reference types

---
+++ ReferenceType/RefTypeArray.fs
@@ -0,0 +1,17 @@
+﻿module RefTypeArray
+ // ref types 
+
+let phrase = ref "Inconsistency"
+
+let totalArray () = 
+    // define an array literal
+    let array = [| 1; 2; 3|]
+    // define a counter
+    let total = ref 0
+    for x in array do
+        // keep running total
+        total := !total + x
+    // print the total
+    printfn "total: %i" !total
+
+totalArray()
---
+++ ReferenceType/RefTypeMutableBetweenSeveralFunctions.fs
@@ -0,0 +1,52 @@
+﻿module RefTypeMutableBetweenSeveralFunctions
+
+// capture inc dec and show functions
+
+let inc, dec, show = 
+    // define the shared state
+    let n = ref 0
+    // a function to increment
+    let inc () =
+        n := !n + 1
+    // a function to decrement
+    let dec () =
+        n := !n - 1
+    // a function to show the current state
+    let show () = 
+        printfn "%i" !n
+
+    // return the functions to the top level
+    inc, dec, show
+
+// test
+inc()
+inc()
+dec()
+show()
+
+// from 4.0 onwards
+
+let inc2, dec2, show2 = 
+    // define the shared state
+    let mutable n =  0
+    // a function to increment
+    let inc2 () =
+        n <- n + 1
+    // a function to decrement
+    let dec2 () =
+        n <- n - 1
+    // a function to show the current state
+    let show2 () = 
+        printfn "%i" n
+
+    // return the functions to the top level
+    inc2, dec2, show2
+
+// test
+inc2()
+inc2()
+dec2()
+show2()
+
+
+
