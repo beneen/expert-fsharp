3279aa0ea5fb09bbfc30d72c12ecaf5f315a1d02
Beneen Tahir <beneen.tahir@gmail.com>
2019-04-10 22:36:13 +0100

    using pipe forward operator on .net libraries

---
+++ PipeForwardOperator/Definition.fs
@@ -0,0 +1,7 @@
+﻿module Definition
+
+let result = 0.5 |> System.Math.Sin
+
+// this technique proves useful when working with .net libraries as it allows the compiler to infer the correct types for the functions parameters
+
+let intList = [1;2;3]
---
+++ PipeForwardOperator/FunctionChaining.fs
@@ -0,0 +1,14 @@
+﻿module FunctionChaining
+
+// grabs list of all methods in memory - takes a few moments to run
+
+let methods = System.AppDomain.CurrentDomain.GetAssemblies()
+                |> List.ofArray
+                |> List.map(fun assm -> assm.GetTypes() )
+                |> Array.concat
+                |> List.ofArray
+                |> List.map (fun t -> t.GetMethods())
+                |> Array.concat
+
+// print the list
+printfn "%A" methods
