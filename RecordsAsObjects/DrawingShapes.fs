92fa4d69323a86eac9b5ad678532fa0baf05759c
Beneen Tahir <beneen.tahir@gmail.com>
2019-04-11 16:20:06 +0100

    drawing the shapes using record types as objects

--- RecordsAsObjects/DrawingShapes.fs
+++ RecordsAsObjects/DrawingShapes.fs
@@ -1,52 +1,52 @@
 ﻿module DrawingShapes
 
+open System
 open System.Drawing
+open System.Windows.Forms
 
 // a shape record that will act as our object
-
 type Shape = 
-    { Reposition: Point -> unit;
-      Draw: unit -> unit}
+    { Reposition: Point -> unit
+      Draw : Graphics -> unit}
 
 // create a new instance of shape
-
-let makeShape initPos draw =
-    // currPos is the internal state of the object
-    let currPos = ref initPos
+let movingShape initPos draw = 
+    // currpos is the internal state of the object
+    let currPos = ref initPos in
     { Reposition = 
-        // the reposition member updates the internal state
+        // the reposition number updates in the internal state
         (fun newPos -> currPos := newPos);
       Draw = 
-        // draw the shape passing the current position to given draw function
-        (fun () -> draw !currPos);}
+      // draw the shape passing the current position
+      // adding graphics object to give a draw function
+        (fun g -> draw ! currPos g);}
 
-// draws a shape and prints out the name and position of the shape
-let draw shape (pos: Point) = 
-    printfn "%s, with x = %i and y = %i"
-        shape pos.X pos.Y
+// create a new circle shape
+let movingCircle initPos diam = 
+    movingShape initPos (fun pos g -> g.DrawEllipse(Pens.Blue, pos.X, pos.Y, diam, diam))
 
-// creates a new circle shape
-let circle initPos = 
-    makeShape initPos (draw "Circle")
-
-// creates new square
-let square initPos = 
-    makeShape initPos (draw "Square")
+// create new square
+let movingSquare initPos diam = 
+    movingShape initPos (fun pos g -> g.DrawRectangle(Pens.Blue, pos.X, pos.Y, diam, diam))
 
 // list of shapes in their initial positions
-
 let shapes = 
-    [ circle (new Point (10,10));
-      square (new Point (30,30))]
+    [ movingCircle (new Point (10, 10)) 20;
+      movingSquare (new Point (30, 30)) 20;
+      movingCircle (new Point (20, 20)) 20
+      movingCircle (new Point (40, 40)) 20]
 
-// draw all the shapes
-let drawShapes() = 
-    shapes |> List.iter (fun s -> s.Draw())
+// create the form to show the items
+let mainForm = 
+    let form = new Form ()
+    let rand = new Random()
+    // add event handler to draw shapes
+    form.Paint.Add (fun e -> shapes |> List.iter (fun s -> s.Draw e.Graphics))
+    // add event handler to move shapes when user clicks the form
+    form.Click.Add(fun e -> shapes |> List.iter (fun s -> 
+                        s.Reposition(new Point(rand.Next(form.Width),
+                        rand.Next(form.Height)))
+                        form.Invalidate()))
+    form
 
-let main() = 
-    drawShapes() // draw shapes
-    // move all the shapes
-    shapes |> List.iter (fun s -> s.Reposition (new Point (40, 40)))
-    drawShapes()
-
-do main()
+do mainForm.ShowDialog() |> ignore
